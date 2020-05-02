7073b28d144d8f2017fc2ae653bd5a05843ee04e
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-05-02 22:39:35 +0000

    ejercicios javascript basic

--- exercises/13-How-much-the-wedding-costs/app.js
+++ exercises/13-How-much-the-wedding-costs/app.js
@@ -1,5 +1,18 @@
 let input = prompt('How many people are coming to your wedding?');
 
 // Your code here
+let price= 0;
+if (input>200){
+    price = 20000;
+}
+else if (input<=200 && input >=99){
+    price = 15000;
+}
+else if (input>=100 && input>50){
+    price = 10000;
+}
+else{
+    price = 4000;
+}
 
 console.log('Your wedding will cost '+price+' dollars');
