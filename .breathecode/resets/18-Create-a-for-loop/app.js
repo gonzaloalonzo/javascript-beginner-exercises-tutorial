4c38fc298ab9affc542b2db1778f86f3c65ec1fa
gonzaloalonzo <gonzaloalonzo@hotmail.com>
2020-05-03 00:20:47 +0000

    ejercicios javascript

--- .breathecode/resets/18-Create-a-for-loop/app.js
+++ .breathecode/resets/18-Create-a-for-loop/app.js
@@ -1,10 +1,7 @@
 /* This exercise should be moved before 17-Your first loop? */
 
 // Declare and write your function here:
-function standardsMaker(){
-    for (i=1; i<=300; i++){
-        console.log ("I will write questions if I'm stuck");
-    }
-}
+
+
 // Calling the function:
 standardsMaker();
--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,14 +1,24 @@
 function getColor(selection)
 {
-	switch(selection){
-		//Add more options here
+    
+    switch(selection){
+        //Add more options here
+        case 'red':
+            return true;
+            break;
+        case 'green':
+            return true
+            break;
+        case 'blue':
+            return true
+            break;
 	    default :
-	    	return false;//returns false because the user picked an unavailable color
-	    break;               
+	    	return false//returns false because the user picked an unavailable color
+	        break;               
 	}
 }
 
-var colorname = window.prompt('What color do you want?');
-var isAvailable = getColor(colorname);
-if(isAvailable) console.log('Good news! That color is available');
-else console.log('We are sorry, that color is not available');
+var colorname = prompt('What color do you want?');
+var isAvailable = getColor(colorname.toLowerCase);
+if(isAvailable){ console.log('Good news! That color is available');}
+else {console.log('We are sorry, that color is not available');}
